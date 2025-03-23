91965990c75948ea2550a3dbc004b01401a97164
John Rogers <johnrogers24@gmail.com>
2025-03-23 00:38:58 +0000

    placeholder shove

--- setup-ghcr.sh
+++ setup-ghcr.sh
@@ -160,6 +160,26 @@ process_template() {
   # Replace placeholders with values - Ubuntu compatible sed
   sed -i "s|\${DOMAIN}|${domain_name}|g" "$output_file"
   sed -i "s|\${ADMIN_EMAIL}|${admin_email}|g" "$output_file"
+  sed -i "s|your_secure_password_here|${db_password}|g" "$output_file"
+  sed -i "s|DB_PASSWORD=your_secure_password_here|DB_PASSWORD=${db_password}|g" "$output_file"
+  sed -i "s|POSTGRES_PASSWORD=your_secure_password_here|POSTGRES_PASSWORD=${db_password}|g" "$output_file"
+  sed -i "s|your_jwt_key_here|${jwt_key}|g" "$output_file"
+  sed -i "s|your_jwt_secret_here|${jwt_secret}|g" "$output_file"
+  sed -i "s|your_admin_password_here|${admin_password}|g" "$output_file"
+  sed -i "s|your_admin_auth_secret_here|${admin_auth_secret}|g" "$output_file"
+  sed -i "s|your_mirotalk_api_key_here|${mirotalk_api_key}|g" "$output_file"
+  sed -i "s|your_mirotalk_api_key_secret_here|${mirotalk_api_key}|g" "$output_file"
+  sed -i "s|your_mirotalk_password_here|${admin_password}|g" "$output_file"  
+  sed -i "s|your_turn_server_credential_here|${turn_password}|g" "$output_file"
+  sed -i "s|your_ome_api_token_here|${ome_api_token}|g" "$output_file"
+  sed -i "s|your_ome_webhook_secret_here|${ome_webhook_secret}|g" "$output_file"
+  sed -i "s|your_minio_root_user_here|${minio_root_user}|g" "$output_file"
+  sed -i "s|your_minio_root_password_here|${minio_root_password}|g" "$output_file"
+  
+  # Replace values in HOST_USERS JSON object
+  sed -i "s|\"password\":\"your_admin_password_here\"|\"password\":\"${admin_password}\"|g" "$output_file"
+
+  # Replace any remaining ${VARIABLES} with their values
   sed -i "s|\${DB_PASSWORD}|${db_password}|g" "$output_file"
   sed -i "s|\${JWT_KEY}|${jwt_key}|g" "$output_file"
   sed -i "s|\${JWT_SECRET}|${jwt_secret}|g" "$output_file"
