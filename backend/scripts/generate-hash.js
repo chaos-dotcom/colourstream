61b8373ce3ef13ecab93d6a32c0efc582b3b3b50
John Rogers <git@shed.gay>
2025-06-26 16:04:39 +0100

    backend deletion

--- backend/scripts/generate-hash.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'admin123';
-bcrypt.hash(password, 12).then(hash => {
-  console.log('Hash for admin123:', hash);
-}); 
--- backend/scripts/verify-hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'admin123';
-const storedHash = '$2a$12$rlF/lsnviWG2kABv0c0q5eoDYF99SetLJ4h1UcqwXgno7Pd3wTwae';
-
-bcrypt.compare(password, storedHash).then(isMatch => {
-  console.log('Password matches:', isMatch);
-}); 
